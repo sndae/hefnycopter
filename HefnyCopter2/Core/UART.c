a5c46c143461577446f9db10835993acf3764a52
Mohammad.Hefny <mohammad.hefny@gmail.com>
2012-12-08 08:03:16 +0000

    -start adding RX command parser

--- HefnyCopter2/Core/UART.c
+++ HefnyCopter2/Core/UART.c
@@ -18,24 +18,114 @@
 #include "../Include/IO_config.h"
 #include "../Include/UART.h"
 
-volatile uint8_t  RXIndex;
+volatile uint8_t  RXIndex=0;
+volatile uint16_t LastRXTime;
+volatile uint8_t RXChar;
+static enum _serial_state {
+    IDLE,
+    HEADER_RX,
+   
+  } c_state = IDLE;
+  
+  
+void ParseCommand();
 
 ISR (USART1_RX_vect)
 {
 	 while ( !(UCSR1A & (1<<RXC1))); 
-	 RXBuffer[RXIndex]=UDR1;
-	 RXIndex=RXIndex+1;
-	 if (RXIndex==10) RXIndex=0;
+	 RXChar = UDR1;
+	 
+	  if ((c_state!=IDLE) && (TCNT1_X - LastRXTime) > 10) 
+	   {
+		   // timeout ..
+		   c_state = IDLE;
+		   RXIndex =0;
+		   DisplayBuffer[4]=0;
+		   memcpy(DisplayBuffer,"OUT",3);
+		   return ;
+	   }
+	   RXBuffer[RXIndex]=RXChar;
+	   switch (c_state)
+	   {
+		   case IDLE:
+		   // Waiting for new command
+			   RXIndex=0;
+			   
+			   
+			   c_state = HEADER_RX;
+			   ATOMIC_BLOCK(ATOMIC_FORCEON)
+			   {
+				 LastRXTime = TCNT1_X;
+			   }
+			  
+		   break;
+		   
+		   case HEADER_RX:
+		   // Copy Data
+				
+			    if (RXIndex==SERIAL_BUFFERSIZE_1)
+				{
+					ParseCommand();
+					c_state = IDLE;
+				}
+				else
+				{
+					ATOMIC_BLOCK(ATOMIC_FORCEON)
+				   {
+					 LastRXTime = TCNT1_X;
+				   }
+				}								
+		   break;
+	   }
+	
+	 RXIndex+=1;
+	 if (RXIndex==SERIAL_BUFFERSIZE) 
+	 {
+		RXIndex=0;
+	 }		
 }
 
 
-
+void ParseCommand ()
+{
+	int8_t CRC=0;
+	for (int i=0;i < SERIAL_BUFFERSIZE -1; ++i)
+	{
+		CRC +=RXBuffer[i];
+	}
+	if (CRC == (int8_t)RXBuffer[SERIAL_CRC_OFFSET])
+	{
+		switch (RXBuffer[SERIAL_HEADER])
+		{
+			case SERIAL_HEADER_DOCMD:
+				switch (RXBuffer[SERIAL_CMD_ID])
+				{
+					case SERIAL_CMD_LED_BLINK:
+						LED_FlashOrangeLED (LED_LONG_TOGGLE,8);
+						DisplayBuffer[4]=0;
+						memcpy(DisplayBuffer,"LED",3);
+					break;
+				}
+			break;
+			case SERIAL_HEADER_SETVALUE:
+				memcpy ((void *) &Config + RXBuffer[SERIAL_DATA_OFFSET],&RXBuffer[SERIAL_DATA_VALUE], (int8_t) RXBuffer[SERIAL_DATA_LENGHT]);
+			break;
+		}
+	}
+	else
+	{
+		DisplayBuffer[4]=0;
+		memcpy(DisplayBuffer,"ERR",3);
+	}		
+	
+	
+}
 
 void UART_Init( unsigned int ubrr)
 {
 	if (Config.RX_mode==RX_mode_UARTMode)
 	{
-		
+		memcpy(DisplayBuffer,"STR",3);
 		RXIndex=0;
 		/*Set baud rate */
 		UBRR1H = (unsigned char)(ubrr>>8);	
@@ -66,6 +156,16 @@ void send_byte(uint8_t u8Data)
 void Send_Data (void * msg, uint8_t len)
 {
 	
+	for (int i=0; i<len;++i)
+	{
+		send_byte (((uint8_t *)msg)[i]);
+	}
+	
+	
+}
+
+void Send_Data_TEST (void * msg, uint8_t len)
+{
 	for (int i=0; i<len;++i)
 	{
 		send_byte (((uint8_t *)msg)[i]);
--- HefnyCopter2/Include/UART.h
+++ HefnyCopter2/Include/UART.h
@@ -9,12 +9,53 @@
 #ifndef UART_H_
 #define UART_H_
 
+/*
+Protocol:
+#CMD#: 1 byte
+		'V'		: SETVALUE:   1 byte
+					DataOffset: 2 bytes		[0-65536]
+					DataLength: 1 byte		[0,1,2,3,4]
+					DataValue:  4 bytes		[Word-32]
+					CHKSUM:		1 byte
+		'C'		: SETCMD
+					CMD:		1 byte
+								0	--		DISARM	
+								1	--		X-MODE
+								2	--		P-MODE
+								3	--		CALIBRATE_ACC
+								4	--		
+								5	--	
+								6	--	
+								7	--	
+								8	--	
+								9	--	
+								
+					DataValue:	6
+					CHKSUM:		1 byte
+*/
 
+//#define SERIAL_OFFSET_ThrottleMin	
+#define SERIAL_CMD_LED_BLINK	0x01
+
+#define SERIAL_HEADER_SETVALUE	'V'
+#define	SERIAL_HEADER_DOCMD		'C'
+#define SERIAL_HEADER			0
+#define SERIAL_CMD_ID			1
+#define SERIAL_DATA_OFFSET		1
+#define SERIAL_DATA_LENGHT		3
+#define SERIAL_DATA_VALUE		4
+#define SERIAL_BUFFERSIZE_1		8
+#define SERIAL_BUFFERSIZE		9
+#define SERIAL_CRC_OFFSET		8
+
+volatile uint8_t  RXBuffer[SERIAL_BUFFERSIZE];
+volatile uint8_t  DisplayBuffer [10]; 
 void UART_Init( unsigned int ubrr);
 
 void send_byte(uint8_t u8Data);
 
 
+void Send_Data_TEST (void * msg, uint8_t len);
 void Send_Data (void * msg, uint8_t len);
 
 //volatile uint8_t UART_Buffer_RTS;
